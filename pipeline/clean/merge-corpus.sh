a635117e37999808aa709d19e6236868bd9ab0c3
onadegibert <odegiber@gmail.com>
2023-09-05 15:47:59 +0300

    Added softlink to fit the filename convention: corpus.source.gz, even in bilinugal scenarios

--- pipeline/clean/merge-corpus.sh
+++ pipeline/clean/merge-corpus.sh
@@ -50,4 +50,10 @@ ${COMPRESSION_CMD} -dc "${tmp}.${src_lang}${trg_lang}.${ARTIFACT_EXT}" | cut -f2
 
 rm -rf "${tmp}"
 
+if [ "${#inputs[@]}" -eq 1 ]; then # If there is only one language pair, create a soft link to fit the filename convention, e.g: corpus.source.gz
+  output_prefix_nolangpair=$(echo "${output_prefix}" | cut -f1 -d".")
+  ln -s  "${output_prefix}.${src_lang}.${ARTIFACT_EXT}"  "${output_prefix_nolangpair}.${src_lang}.${ARTIFACT_EXT}"
+  ln -s  "${output_prefix}.${trg_lang}.${ARTIFACT_EXT}"  "${output_prefix_nolangpair}.${trg_lang}.${ARTIFACT_EXT}"
+fi
+
 echo "###### Done: Merging parallel datasets"
