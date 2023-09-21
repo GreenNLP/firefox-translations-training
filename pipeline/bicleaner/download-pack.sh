294bd169935da7b29e5a6fd44916295bb5f62d16
TommiNieminen <nieminentommi@hotmail.com>
2023-09-21 23:30:00 +0300

    fixed bicleaner-ai to work with v2 models

--- pipeline/bicleaner/download-pack.sh
+++ pipeline/bicleaner/download-pack.sh
@@ -19,42 +19,56 @@ invalid_url() {
   wget -S --spider -o - $1 | grep -q '404 Not Found'
 }
 
+# compatibility fix for earlier bicleaner-ai versions, they don't allow download path
+export HF_HOME=${download_path}
+
 if [ "${type}" == 'bicleaner-ai' ]; then
-    #bicleaner-ai v2.0 full models are only available through Hugging Face.
-    #TODO: change this code to download models from HF 
-    url="https://github.com/bitextor/bicleaner-ai-data/releases/download/v1.0"
-    prefix="full-"
-    extension="tgz"
+    #bicleaner-ai v2.0 full models are only available through Hugging Face.    
+    bicleaner-ai-download ${SRC} ${TRG} full ${download_path} --debug
+    url="bicleaner-ai-download"
+    #compatibility fix for earlier bicleaner-ai versions
+    if [ ! -f "${download_path}/config.json" ]; then
+      config_path=$(find ${download_path} -name "config.json")
+      model_dir=$(dirname ${config_path})
+      # the snapshot files are soft links to blobs in the HF cache
+      cp --dereference "${model_dir}/"* ${download_path}
+      rm -r "${download_path}/hub"
+      # the bicleaner script checks for folder with model lang dir, create it
+      model_src=$(grep "source_lang:" "${download_path}"/metadata.yaml | cut -f2 -d ':' | tr -d ' ') 
+      model_trg=$(grep "target_lang:" "${download_path}"/metadata.yaml | cut -f2 -d ':' | tr -d ' ')
+      mkdir "${download_path}/${model_src}-${model_trg}"
+    fi
 elif [ "${type}" == 'bicleaner' ]; then
     url="https://github.com/bitextor/bicleaner-data/releases/latest/download"
     prefix=""
     extension="tar.gz"
+    echo "### Downloading ${type} language pack ${url}"
+
+    if invalid_url "${url}/${prefix}${SRC}-${TRG}.${extension}"; then
+      echo "### ${SRC}-${TRG} language pack does not exist, trying ${TRG}-${SRC}..."
+      if invalid_url "${url}/${prefix}${TRG}-${SRC}.${extension}"; then
+        echo "### ${TRG}-${SRC} language pack does not exist"
+        exit 1
+      else
+        lang1=$TRG
+        lang2=$SRC
+      fi
+    else
+      lang1=$SRC
+      lang2=$TRG
+    fi
+
+
+    wget -P "${download_path}" "${url}/${prefix}${lang1}-${lang2}.${extension}"
+    tar xvf "${download_path}/${prefix}${lang1}-${lang2}.${extension}" -C "${download_path}" --no-same-owner
+    mv "${download_path}/${lang1}-${lang2}"/* "${download_path}/"
+    rm "${download_path}/${prefix}${lang1}-${lang2}.${extension}"
+
 else
   echo "Unsupported type: ${type}"
   exit 1
 fi
 
-echo "### Downloading ${type} language pack ${url}"
-
-if invalid_url "${url}/${prefix}${SRC}-${TRG}.${extension}"; then
-  echo "### ${SRC}-${TRG} language pack does not exist, trying ${TRG}-${SRC}..."
-  if invalid_url "${url}/${prefix}${TRG}-${SRC}.${extension}"; then
-    echo "### ${TRG}-${SRC} language pack does not exist"
-    exit 1
-  else
-    lang1=$TRG
-    lang2=$SRC
-  fi
-else
-  lang1=$SRC
-  lang2=$TRG
-fi
-
-
-wget -P "${download_path}" "${url}/${prefix}${lang1}-${lang2}.${extension}"
-tar xvf "${download_path}/${prefix}${lang1}-${lang2}.${extension}" -C "${download_path}" --no-same-owner
-mv "${download_path}/${lang1}-${lang2}"/* "${download_path}/"
-rm "${download_path}/${prefix}${lang1}-${lang2}.${extension}"
 
 
 echo "### ${type} language pack ${url} is downloaded"
--- pipeline/bicleaner/packs.py
+++ pipeline/bicleaner/packs.py
@@ -1,7 +1,8 @@
 import requests
 
 bicleaner = 'https://github.com/bitextor/bicleaner-data/releases/latest/download'
-bicleaner_ai = 'https://github.com/bitextor/bicleaner-ai-data/releases/download/v1.0'
+#bicleaner_ai = 'https://github.com/bitextor/bicleaner-ai-data/releases/download/v1.0'
+bicleaner_ai = 'https://huggingface.co/bitextor/bicleaner-ai'
 
 
 def _exists(url):
@@ -9,7 +10,7 @@ def _exists(url):
 
 
 def find(src, trg):
-    if _exists(f'{bicleaner_ai}/full-{src}-{trg}.tgz') or _exists(f'{bicleaner_ai}/full-{trg}-{src}.tgz'):
+    if _exists(f'{bicleaner_ai}-full-{src}-{trg}') or _exists(f'{bicleaner_ai}-full-{trg}-{src}'):
         return 'bicleaner-ai'
 
     if _exists(f'{bicleaner}/{src}-{trg}.tar.gz') or _exists(f'{bicleaner}/{trg}-{src}.tar.gz'):
