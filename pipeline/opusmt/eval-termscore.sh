1e01a25d0d134af228dd541671a88933ac04345e
Tommi Nieminen <tommi.nieminen@helsinki.fi>
2024-04-11 17:04:25 +0300

    modified eval script

--- pipeline/opusmt/eval-termscore.sh
+++ pipeline/opusmt/eval-termscore.sh
@@ -24,7 +24,19 @@ args=( "${@:10}" )
 
 mkdir -p "$(basename "${res_prefix}")"
 
-# Check whether this is a term model
+# generate termless translations the same way for both base and term models
+cat "${eval_src}" |  perl -pe 's/augmentsymbol0 (.*?) augmentsymbol1 .*? augmentsymbol2/\1/g' |
+  tee "${res_prefix}.eval.noterms.${src}" |
+  "${MARIAN}"/marian-decoder \
+  -c "${decoder_config}" \
+  --quiet \
+  --quiet-translation \
+  --log "${res_prefix}.log" \
+  --models ${models} \
+  --vocabs "${vocab}" "${vocab}" \
+  "${args[@]}" > "${res_prefix}.eval.noterms.${trg}"
+
+# Check whether this is a term model, preprocess source accordingly
 model_base_dir=$(basename $(dirname ${models}))
 term_model_regex="^.*term-([a-z-]+)-[0-9]+-[0-9]+"
 if [[ $model_base_dir =~ $term_model_regex ]]; then
@@ -39,7 +51,7 @@ if [[ $model_base_dir =~ $term_model_regex ]]; then
       --vocabs "${vocab}" "${vocab}" \
       "${args[@]}" > "${res_prefix}.eval.${trg}"
 else
-  cat "${eval_src}" |  perl -pe 's/augmentsymbol0 (.*?) augmentsymbol1 .*? augmentsymbol2/\1/g' |
+  cat "${eval_src}" |  perl -pe 's/augmentsymbol0 (.*?) augmentsymbol1 (.*?) augmentsymbol2/\2/g' |
   tee "${res_prefix}.eval.${src}" |
     "${MARIAN}"/marian-decoder \
       -c "${decoder_config}" \
@@ -51,7 +63,13 @@ else
       "${args[@]}" > "${res_prefix}.eval.${trg}"
 fi
 
+# Evaluate term output
 python 3rd_party/soft-term-constraints/src/sgm_generator.py --hypothesis_only --input_trg_path "${res_prefix}.eval.${trg}" --source_lang_code ${src} --target_lang_code ${trg} --set_id evalset --output_trg_path "${res_prefix}.eval.sgm.${trg}"
 
-python 3rd_party/terminology_evaluation/evaluate_term_wmt.py --language ${trg} --hypothesis "${res_prefix}.eval.sgm.${trg}" --source "${sgm_source}" --target_reference "${sgm_ref}" --log "${res_prefix}.score" --EXACT_MATCH True --WINDOW_OVERLAP True --MOD_TER True --TER True --COMET True --match_counts_path "${res_prefix}.match_counts" 
+python 3rd_party/terminology_evaluation/evaluate_term_wmt.py --language ${trg} --hypothesis "${res_prefix}.eval.sgm.${trg}" --source "${sgm_source}" --target_reference "${sgm_ref}" --log "${res_prefix}.score" --EXACT_MATCH True --WINDOW_OVERLAP True --MOD_TER True --TER True --COMET True --match_counts_path "${res_prefix}.match_counts" --comet_model_path ../comet_models/models--Unbabel--wmt22-comet-da/snapshots/371e9839ca4e213dde891b066cf3080f75ec7e72/checkpoints/model.ckpt 
+
+# Evaluate termless output
+python 3rd_party/soft-term-constraints/src/sgm_generator.py --hypothesis_only --input_trg_path "${res_prefix}.eval.noterms.${trg}" --source_lang_code ${src} --target_lang_code ${trg} --set_id evalset --output_trg_path "${res_prefix}.eval.noterms.sgm.${trg}"
+
+python 3rd_party/terminology_evaluation/evaluate_term_wmt.py --language ${trg} --hypothesis "${res_prefix}.eval.noterms.sgm.${trg}" --source "${sgm_source}" --target_reference "${sgm_ref}" --log "${res_prefix}.noterms.score" --EXACT_MATCH True --WINDOW_OVERLAP True --MOD_TER True --TER True --COMET True --match_counts_path "${res_prefix}.noterms.match_counts" --comet_model_path ../comet_models/models--Unbabel--wmt22-comet-da/snapshots/371e9839ca4e213dde891b066cf3080f75ec7e72/checkpoints/model.ckpt 
 
