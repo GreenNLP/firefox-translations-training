18497a661943190cc3b204b5f22975a05ae42955
Ona De Gibert Bonet <degibert@mahti-login11.mahti.csc.fi>
2024-10-04 11:46:09 +0300

    added strategy to remove empty lines

--- pipeline/alignment/generate-alignment-tsv.sh
+++ pipeline/alignment/generate-alignment-tsv.sh
@@ -27,6 +27,17 @@ mkdir -p "${dir}"
 corpus_src="${corpus_prefix}.${SRC}.gz"
 corpus_trg="${corpus_prefix}.${TRG}.gz"
 
+test -s "${dir}/cleaned_empty_lines" ||
+  echo "### Removing empty target lines"
+  paste <(pigz -dc "${corpus_src}") <(pigz -dc "${corpus_trg}") | sed 's/\t/ ||| /' >"${dir}/corpus"
+  awk -F ' \\|\\|\\| ' '$1!="" && $2!=""' "${dir}/corpus" > "${dir}/corpus_dedup"
+
+  echo "### Splitting corpus back into source and target files and overwriting source and target files"
+  awk -F' \\|\\|\\| ' '{print $1}' "${dir}/corpus_dedup" | pigz -c > "${corpus_src}"
+  awk -F' \\|\\|\\| ' '{print $2}' "${dir}/corpus_dedup" | pigz -c > "${corpus_trg}"
+  rm "${dir}/corpus"
+  rm "${dir}/corpus_dedup"
+  touch "${dir}/cleaned_empty_lines"
 
 echo "### Subword segmentation with SentencePiece"
 test -s "${dir}/corpus.spm.${SRC}.gz" ||
